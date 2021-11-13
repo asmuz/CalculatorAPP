02fe470f159f6c54b4e763087418914b439c5406
AsmuZ <asmuz@mail.ru>
2021-11-14 02:09:15 +0300

    Init Logic Class

---
+++ app/src/main/java/com/example/calculator/Logic.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+
+public class Logic {
+
+    private int firstArg;
+    private int SecondArg;
+
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Logic logic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logic = new Logic();
+
     }
+
+
+
 }
